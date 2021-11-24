b1e76a1fa5a11c15801d5701e22af36520f7fcfc
EliasBelov <71308871+EliasBelov@users.noreply.github.com>
2021-11-24 07:52:28 +0300

    temp commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
 
-# Create your models here.
+    def __str__(self):
+        return self.title
